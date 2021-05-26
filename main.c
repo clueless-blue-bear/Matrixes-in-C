1df442ca39833644ee798d272f8830e234fad1f2
clueless-blue-bear <clueless_blue_bear@outlook.com>
2021-05-27 00:26:39 +0200

    Minor Rework
    Added option to use "+" and "-" instead of 1 and 2 to perform arithmetic operations. Moved creation of the main matrix to the main function.

--- main.c
+++ main.c
@@ -3,6 +3,25 @@
 #include "OperationsOnMatrixes.h"
 int main()
 {
-    BasicRowOperations();
+    unsigned int m,n;
+    float tmpCellValue;
+
+        printf("Please state the number of rows of your matrix: ");
+            scanf("%d",&m);
+        printf("\nPlease state the number of columns of your matrix: ");
+            scanf("%d",&n);
+
+        float matrix[m][n];
+
+        for(int i=0;i < m; i=i+1)
+        {
+            for(int j = 0;j < n;j=j+1)
+            {
+                printf("Please input the number in row number %d , column number %d \n",i+1,j+1);
+                    scanf("%f",&tmpCellValue);
+                    matrix[i][j] = tmpCellValue;
+            }
+        }
+        BasicRowOperations(m,n,matrix);
     return 0;
 }
